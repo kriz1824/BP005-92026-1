4c998a3befb3e4b0455a1051b67c1f2db2056f4e
kriz1824 <cristophersang@gmail.com>
2026-03-17 19:29:57 -0500

    Add readm.c to read and display height input

---
+++ CORTE1/c/readm.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+
+float estatura;
+
+
+printf("Ingrese su estatura en metros: ");
+
+scanf("%f", &estatura);
+
+
+printf("La estatura ingresada es: %.2f m\n", estatura);
+
+return 0;
+}
