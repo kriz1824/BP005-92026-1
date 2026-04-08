92bb60708866226394fb1f7b76c8e70587becd63
kriz1824 <cristophersang@gmail.com>
2026-04-08 13:55:48 -0500

    Create p3.c

---
+++ corte2/c/practicas/tarea1/p3.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+
+int main() {  
+    int i = 1; //variable control
+
+    do {
+       printf("%d\n", i); //muestra el valor actual
+       i++; //actualiza la variable
+     } while (i  <= 5); //verifica la condicion al final 
+
+     return 0;
+
+}
+
