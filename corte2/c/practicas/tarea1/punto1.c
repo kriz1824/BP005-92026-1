f21d0ea9f57d97523b562f51e6ce8daf89df6f8d
kriz1824 <cristophersang@gmail.com>
2026-04-08 13:54:32 -0500

    Add simple loop example in punto1.c

---
+++ corte2/c/practicas/tarea1/punto1.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+int main () {
+   int i = 1; //variable control
+
+   while (i <= 5) { //mientras i sea < que 5
+     printf("actualmente en %d\n", i); //muestra valor actual
+     i++; //actualiza la variable
+   }
+
+   return 0;
+}
+
